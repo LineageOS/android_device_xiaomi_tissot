b32c85b0eaab878e3a1163043af3f7582b73a4a5
TheScarastic <warabhishek@gmail.com>
2018-05-10 18:28:46 +0530

    tissot: create symlink of wlan_mac.bin in vendor/firmware

--- Android.mk
+++ Android.mk
@@ -27,4 +27,15 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tissot)
 include $(call all-makefiles-under,$(LOCAL_PATH))
+
+WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/wlan_mac.bin
+$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
+	@echo "WCNSS MAC bin link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /persist/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
+
+
 endif
