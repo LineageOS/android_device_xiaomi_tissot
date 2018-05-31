a069c916555a4a433f5c6e12b090a48df4fe2f5f
TheScarastic <warabhishek@gmail.com>
2018-05-31 19:53:29 +0530

    tissot: fix copying of sensor_def_qcomdev.conf
    Change-Id: I2f6d8494309b8bb6796ea094b3bbf558ec4fecf2

--- device.mk
+++ device.mk
@@ -70,7 +70,7 @@ PRODUCT_PACKAGES += \
     init.tissot.rc
 
 # Sensors
-PRODUCT_PACKAGES += \
+PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
 
 # Update engine
