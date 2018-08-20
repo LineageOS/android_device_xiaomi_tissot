e86107ebecb5ced4d2239a8ee38c5386b247ced8
Bruno Martins <bgcngm@gmail.com>
2018-08-20 13:27:09 +0530

    tissot: Consider lineage_mido product via the AOSP way
    Change-Id: I4e17dffe6d6187c34425dcb4eea77bc9ea9e2dd6

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -14,13 +14,24 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/tissot/full_tissot.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from mido device
+$(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := tissot
 PRODUCT_NAME := lineage_tissot
 BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Mi A1
+PRODUCT_MANUFACTURER := Xiaomi
+TARGET_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
