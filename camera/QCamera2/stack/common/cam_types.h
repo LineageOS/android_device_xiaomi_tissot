45607388c45cd7ff245299731f94b019654b1757
Artem Borisov <dedsa2002@gmail.com>
2019-02-02 19:37:37 +0100

    tissot: camera: Hacks to make camera HAL compatible with blobs
    Change-Id: I9e95dbcdfde3285ae21ecb46b02215b0c98d6f4c

--- camera/QCamera2/stack/common/cam_types.h
+++ camera/QCamera2/stack/common/cam_types.h
@@ -51,7 +51,7 @@
 #define CEILING4(X)  (((X) + 0x0003) & 0xFFFC)
 #define CEILING2(X)  (((X) + 0x0001) & 0xFFFE)
 
-#define MAX_ZOOMS_CNT 91
+#define MAX_ZOOMS_CNT 101
 #define MAX_SIZES_CNT 40
 #define MAX_EXP_BRACKETING_LENGTH 32
 #define MAX_ROI 10
@@ -1049,6 +1049,7 @@ typedef struct {
         cam_coordinate_type_t coordinate[MAX_ROI];
         uint32_t aec_roi_idx[MAX_ROI];
     } cam_aec_roi_position;
+    uint16_t xiaomi_reserved;
 } cam_set_aec_roi_t;
 
 typedef struct {
@@ -2152,6 +2153,7 @@ typedef enum {
     /* parameters added for related cameras */
     /* fetch calibration info for related cam subsystem */
     CAM_INTF_PARM_RELATED_SENSORS_CALIBRATION,
+    XIAOMI_01,
     /* focal length ratio info */
     CAM_INTF_META_AF_FOCAL_LENGTH_RATIO,
     /* crop for binning & FOV adjust */
@@ -2170,6 +2172,7 @@ typedef enum {
     CAM_INTF_PARM_FLIP,
     /*Frame divert info from ISP*/
     CAM_INTF_BUF_DIVERT_INFO,
+    XIAOMI_02,
     /* Use AV timer */
     CAM_INTF_META_USE_AV_TIMER,
     CAM_INTF_META_EFFECTIVE_EXPOSURE_FACTOR,
@@ -2234,6 +2237,8 @@ typedef enum {
     CAM_INTF_META_SPOT_LIGHT_DETECT,
     /* HAL based HDR*/
     CAM_INTF_PARM_HAL_BRACKETING_HDR,
+    XIAOMI_03,
+    XIAOMI_04,
     CAM_INTF_PARM_MAX
 } cam_intf_parm_type_t;
 
