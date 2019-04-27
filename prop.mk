59d62b0dd7ced5779e62c3d89c7c41f33269e48d
Artem Borisov <dedsa2002@gmail.com>
2019-04-27 16:11:22 +0530

    tissot: Add fluence properties
    * Fluence should be disabled as per stock
    
    Change-Id: Ife676b70d03d6c17395ddbf4db3ff8c54e726573

--- prop.mk
+++ prop.mk
@@ -1,3 +1,10 @@
+# Audio
+PRODUCT_PROPERTY_OVERRIDES += \
+persist.vendor.audio.fluence.speaker=true \
+persist.vendor.audio.fluence.voicecall=true \
+persist.vendor.audio.fluence.voicerec=false \
+ro.vendor.audio.sdk.fluencetype=none
+
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
