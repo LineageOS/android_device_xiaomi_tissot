2f2560d95ffbe8c4ae4c6dcd19ac539e4fdae61e
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-01-25 19:40:30 +0100

    tissot: Add prebuilt protobuf from sdk29
    Change-Id: I980a9e66cc0f99b5292210428c6ae9737e5b3969

--- extract-files.sh
+++ extract-files.sh
@@ -5,6 +5,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib64/libril-qc-hal-qmi.so)
+            "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
