5845173e7fd3fb42a421b372a932fc21fec7007c
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-14 17:20:26 +0200

    tissot: Commonize WCNSS_MAC_SYMLINK
    Change-Id: If280adae81f68ae7d4417caa1a168e07c43bbf80

--- Android.mk
+++ Android.mk
@@ -7,16 +7,7 @@
 LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),tissot)
+
 include $(call all-makefiles-under,$(LOCAL_PATH))
-
-WCNSS_MAC_SYMLINK := $(TARGET_OUT_VENDOR)/firmware/wlan/prima/wlan_mac.bin
-$(WCNSS_MAC_SYMLINK): $(LOCAL_INSTALLED_MODULE)
-	@echo "WCNSS MAC bin link: $@"
-	@mkdir -p $(dir $@)
-	@rm -rf $@
-	$(hide) ln -sf /mnt/vendor/persist/$(notdir $@) $@
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WCNSS_INI_SYMLINK) $(WCNSS_MAC_SYMLINK)
-
 
 endif
