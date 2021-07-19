88018c155ff9dcd1dc709e08d7373d56eeda95f5
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-07-19 09:43:00 +0200

    tissot: Decommonize fingerprint permission XML
    Change-Id: I66213a2b79293816b5e55a21291d7e3720c89df0

--- device.mk
+++ device.mk
@@ -65,6 +65,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Properties
 -include device/xiaomi/tissot/prop.mk
 
