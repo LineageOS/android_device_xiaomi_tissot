7cbaa9121afbba995770e09627a0d66014b62a96
ItsVixano <vixanonew@gmail.com>
2021-10-08 16:57:27 +0200

    tissot: camera: Drop HALv1 once and for for all
    * Has been deprecated on aosp-12
    
    Change-Id: I02d5f320db9243c04d89d58fef775dfe33fe4ac7

--- prop.mk
+++ prop.mk
@@ -8,5 +8,5 @@ ro.vendor.audio.sdk.fluencetype=fluence
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap \
-persist.camera.dual.camera=0
-
+persist.camera.dual.camera=0 \
+persist.vendor.camera.HAL3.enabled=1
