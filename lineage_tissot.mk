57dbbdec34d752b1473ce8473efd6e06f5f77411
Martin <martin.rahnefeld@googlemail.com>
2022-11-29 10:03:25 +0100

    Update fingerprint to enable Google Familylink
    Change-Id: I75e9b9539f1e465999d1f50320d5b9e4fb0784fd

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -26,7 +26,7 @@ TARGET_VENDOR := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="tissot-user 8.0.0 OPR1.170623.026 8.1.10 release-keys"
+    PRIVATE_BUILD_DESC="tissot-user 9 PKQ1.180917.001 V10.0.24.0.PDHMIXM release-keys"
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
-BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:8.0.0/OPR1.170623.026/8.1.10:user/release-keys"
+BUILD_FINGERPRINT := "xiaomi/tissot/tissot_sprout:9/PKQ1.180917.001/V10.0.24.0.PDHMIXM:user/release-keys"
