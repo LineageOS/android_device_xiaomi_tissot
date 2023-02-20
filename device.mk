dceef02626bbcdc2cbc5e99a1abf66cb4d1aa652
Giovanni Ricca <giovanniricca@proton.me>
2023-02-20 11:12:56 +0100

    tissot: rootdir: Kill goodix hax
    * Unused since we switched to oss biometrics hal
    
    Change-Id: I44811aed5475ff1be3a7dea1c1b3d6827ffee56d

--- device.mk
+++ device.mk
@@ -72,11 +72,6 @@ PRODUCT_PACKAGES += \
 # Properties
 -include device/xiaomi/tissot/prop.mk
 
-# Ramdisk
-PRODUCT_PACKAGES += \
-    init.goodix.sh \
-    init.tissot.rc
-
 # Sensors
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sensors/sensor_def_qcomdev.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/sensor_def_qcomdev.conf
