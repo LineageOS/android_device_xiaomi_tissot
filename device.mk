a3141e7410476aacc4500308c880e217d6e2a5ed
Giovanni Ricca <giovanniricca@proton.me>
2023-02-20 11:10:58 +0100

    tissot: rootdir: Commonize fingerprint permissions
    Change-Id: Idebf7c4c22cfd0963792580c344e699dc2d0f4f1

--- device.mk
+++ device.mk
@@ -59,9 +59,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953
 
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0-impl \
