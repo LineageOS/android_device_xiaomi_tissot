85fc3ab7592b04058b80ae540d12f2abf06e1249
Giovanni Ricca <giovanniricca@proton.me>
2023-10-28 11:26:27 +0200

    tissot: Drop AVB 1.0 flags
    * They got deprecated on Android 14 [1]. We actually never configured verity/avb properly anyway
    
    [1] https://android-review.googlesource.com/c/platform/build/+/2176952
    
    Change-Id: I67d196cebeedd05a64b2e4c61bf9a7771f04ca5d

--- device.mk
+++ device.mk
@@ -109,9 +109,5 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     update_engine_client
 
-# Verity
-PRODUCT_SYSTEM_VERITY_PARTITION := /dev/block/platform/soc/7824900.sdhci/by-name/system
-$(call inherit-product, build/target/product/verity.mk)
-
 # Inherit the proprietary files
 $(call inherit-product, vendor/xiaomi/tissot/tissot-vendor.mk)
