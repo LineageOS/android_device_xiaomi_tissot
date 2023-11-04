a7849eb0a95d721c9aef914adcecf513c7958e40
Yumi Yukimura <me.cafebabe@gmail.com>
2023-11-05 03:40:53 +0800

    tissot: Switch to patchelf v0.17.2 for patching our blobs
    [rktdnt]: Adapt with tissot blobs
    
    * All our fingerprint blobs are quite nasty, patchelf v0.9 could
      easily break loading of these fingerprint blobs
    * The host patchelf which I previously used for patching these
      fingerprint blobs is v0.14.3 (Debian 0.14.3-1+b1)
    
    Change-Id: I8c3976f99c26314c97b2f1384a9091c7d7c29566

--- extract-files.sh
+++ extract-files.sh
@@ -11,19 +11,19 @@ function blob_fixup() {
             "${PATCHELF}" --replace-needed "libprotobuf-cpp-full.so" "libprotobuf-cpp-full-v29.so" "${2}"
             ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
-            "${PATCHELF_0_8}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
+            "${PATCHELF_0_17_2}" --remove-needed "libprotobuf-cpp-lite.so" "${2}"
             ;;
 	vendor/lib/libmmcamera2_sensor_modules.so)
 	    sed -i 's|/system/etc/camera/|/vendor/etc/camera/|g' "${2}"
 	    ;;
 	vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
-            "${PATCHELF_0_8}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
+            "${PATCHELF_0_17_2}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
 	    ;;
     esac
 
     # For all ELF files
     if [[ "${1}" =~ ^.*(\.so|\/bin\/.*)$ ]]; then
-        "${PATCHELF}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
+        "${PATCHELF_0_17_2}" --replace-needed "libstdc++.so" "libstdc++_vendor.so" "${2}"
     fi
 }
 
