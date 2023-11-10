3a054a7f3118b5349fd7e0332b74e1a1e5a2491f
Giovanni Ricca <giovanniricca@proton.me>
2023-11-10 20:50:16 +0000

    tissot: Target is legacy
    Change-Id: If5eb7005e6b7c793289bb3008ac564c335791b21

--- lineage_tissot.mk
+++ lineage_tissot.mk
@@ -12,6 +12,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Inherit from tissot device
+TARGET_IS_LEGACY := true
 $(call inherit-product, device/xiaomi/tissot/device.mk)
 
 # Device identifier. This must come after all inclusions
